cbbc189d8f75bf92c3f27c89048985d6888233dc
Majd Abboud <majdabboud81@gmail.com>
2018-02-15 00:25:37 +0200

    Added string variable

--- AtmZ/main.py
+++ AtmZ/main.py
@@ -7,19 +7,19 @@ class Atm:
     def withdraw(self, request):
 
         result = self.balance
-
-        print "Welcome to " + self.bank_name
-        print "Current balance = " + str(self.balance)
-        print "======================================="
+        string = "======================================="
+        print ("Welcome to " + self.bank_name)
+        print ("Current balance = " + str(self.balance))
+        print (string)
 
         if request > self.balance:
-            print "SORRY!! Can't give you all this money ! ! ! "
-            print "======================================="
+            print ("SORRY!! Can't give you all this money ! ! ! ")
+            print (string)
             result = self.balance
 
         elif request <= 0:
-            print "SORRY !!! Please request more than 0"
-            print "======================================="
+            print ("SORRY !!! Please request more than 0")
+            print (string)
             result = self.balance
 
         else:
@@ -29,24 +29,24 @@ class Atm:
 
                 if request >= 100:
                     request -= 100
-                    print "give 100"
+                    print ("give 100")
 
                 elif request >= 50:
                     request -= 50
-                    print "give 50"
+                    print ("give 50")
 
                 elif request >= 10:
                     request -= 10
-                    print "give 10"
+                    print ("give 10")
 
                 elif request >= 5:
                     request -= 5
-                    print "give 5"
+                    print ("give 5")
 
                 elif request < 5:
-                    print "give " + str(request)
+                    print ("give " + str(request))
                     request = 0
-            print "======================================="
+            print (string)
             return self.balance
 
         return result
