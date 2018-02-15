0699900637a4d79af77e2cc84cad8d1aacff8196
Majd Abboud <majdabboud81@gmail.com>
2018-02-15 16:39:16 +0200

    added Forum

---
+++ Forums/models.py
@@ -0,0 +1,11 @@
+class Members:
+    def __init__(self, member_name, member_age):
+        self.name = member_name
+        self.age = member_age
+
+class Post:
+    def __init__(self,post_title,post_content):
+        self.title = post_title
+        self.content = post_content
+
+
