a676f8e892082688792499912ac5b592d0ea4c18
Majd Abboud <majdabboud81@gmail.com>
2018-02-15 15:58:08 +0200

    process request

--- AtmZ/main.py
+++ AtmZ/main.py
@@ -26,31 +26,31 @@ class Atm:
             self.withdraw_list.append(request)
             self.balance = self.balance - request
 
-        def calculator (request):
-            while request > 0:
+            def process_request(request):
+                while request > 0:
 
-                if request >= 100:
-                    request -= 100
-                    print ("give 100")
+                    if request >= 100:
+                        request -= 100
+                        print ("give 100")
 
-                elif request >= 50:
-                    request -= 50
-                    print ("give 50")
+                    elif request >= 50:
+                        request -= 50
+                        print ("give 50")
 
-                elif request >= 10:
-                    request -= 10
-                    print ("give 10")
+                    elif request >= 10:
+                        request -= 10
+                        print ("give 10")
 
-                elif request >= 5:
-                    request -= 5
-                    print ("give 5")
+                    elif request >= 5:
+                        request -= 5
+                        print ("give 5")
 
-                elif request < 5:
-                    print ("give " + str(request))
-                    request = 0
+                    elif request < 5:
+                        print ("give " + str(request))
+                        request = 0
+            process_request(request)
             print (string)
             return self.balance
-        return result
 
     def show_withdrawls(self):
         for withdrawl in self.withdraw_list:
