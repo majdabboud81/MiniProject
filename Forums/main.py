2ec904785b7fa418994d42a45bdca71b33a0f0e2
Majd Abboud <majdabboud81@gmail.com>
2018-02-15 17:24:58 +0200

    test push

--- Forums/main.py
+++ Forums/main.py
@@ -6,4 +6,3 @@ member2 = models.Members("Rida", 28)
 post1 = models.Post("Post 1", "Some contect whatever")
 post2 = models.Post("Post2", "Som content whatever")
 post3 = models.Post("Post2", "Som content whatever")
-
