2b29c0dba0776e03f8169aec4972486d534e7c65
Majd Abboud <majdabboud81@gmail.com>
2018-02-16 02:30:22 +0200

    added post class

--- Forums/main.py
+++ Forums/main.py
@@ -9,11 +9,15 @@ member_store = store.Members_Store()
 member_store.add(member1)
 member_store.add(member2)
 
-print (member_store.get_all())
 
 #POSTS INSTANCES ------>>>>
 
-post_store = store.Posts_Store()
+
 post1 = models.Post("Post1", "Some contect whatever")
 post2 = models.Post("Post2", "Som content whatever")
 post3 = models.Post("Post2", "Som content whatever")
+
+post_store = store.Posts_Store()
+post_store.add(post1)
+post_store.add(post2)
+post_store.add(post3)
