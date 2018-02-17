680c15499d1c03fd57eef9b2d0b2a21bbffc0e16
majdabboud81 <36302234+majdabboud81@users.noreply.github.com>
2018-02-17 03:36:58 +0200

    Update store.py

--- Forums/store.py
+++ Forums/store.py
@@ -13,7 +13,7 @@ class MembersStore:
         MembersStore.last_id += 1
 
     def get_by_id(self, id):
-        all_members = MembersStore.get_all(self)
+        all_members = self.get_all()
         result = None
         for member in all_members:
             if member.id == id: # replaced is cuz The operators is and is not test for object identity: x is y is true if and only if x and y are the same object.
@@ -49,7 +49,7 @@ class PostsStore:
         PostsStore.last_id += 1
 
     def get_by_id(self, id):
-        all_posts = PostsStore.get_all(self)
+        all_posts = self.get_all()
         result = None
         for post in all_posts:
             if post.id == id:
