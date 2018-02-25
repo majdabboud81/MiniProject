b21600a2f0204c3f11961041772112911ad177d0
Majd Abboud <majdabboud81@gmail.com>
2018-02-25 14:18:49 +0200

    Flask added

---
+++ flask-demo/test.py
@@ -0,0 +1,13 @@
+import Flask
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def home():
+    return "Hi there"
+
+@app.route("/SayHello/<name>")
+def say_hello(name):
+    return "Hello %s" % name
+app.run()
+
