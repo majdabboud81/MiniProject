d6e2d68e7a7c0e2c3c7f1fe9f599746eac49e7db
Majd Abboud <majdabboud81@gmail.com>
2018-02-28 16:14:26 +0200

    Added posts

--- Forums/app.py
+++ Forums/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
-
+import models, store
 
 app = Flask(__name__)
+post_store = store.PostsStore()
+post_store.add(models.Post("Life", "Life is alawys great"))
+post_store.add(models.Post("Sunshine", "Sunshine is amazing"))
+
+@app.route("/")
 @app.route("/")
 @app.route("/index")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts = post_store.get_all())
 
 app.run()
